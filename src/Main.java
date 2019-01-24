fd8bdc2b08b4c29cc6f0d6a942014bb462c85b5a
Igor <igor.dumitraskovic91@gmail.com>
2019-01-24 20:05:19 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("HelloWorld");
+        System.out.println("Igor");
+        
+        String user = "Igor Dumitraskovic";
+        
+        Car audi = new Car();
+        audi.printVariables();
+    }
+    
+}
